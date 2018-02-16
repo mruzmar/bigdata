4d0366542cc0d648d1a817469cbf613597cbec4f
miguelangel <mruz@onwhyon.com>
2018-02-16 23:01:13 +0100

    Ingesta twitter

---
+++ python/jupyter/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'C1voYrjnzAcwjRU1k8SmL72vK'
+consumer_secret = 'Uvk3ljIL8duuLM7XxLXrlXBBEkHDl040dxgJox8p9pdJVcxRjh'
+access_key = '15002884-QxpvkW1jNTQ3YpO9Vn4yW9Cx8n7UyvwdVzVkrmMbw'
+access_secret = 'eaFWhbH2Mn1trxlYoKlLZsk3nzkk4wsiaFTyIkDbLEYob'
