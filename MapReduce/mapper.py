523e01951151a5ec107db50e8d028ddba05576b5
Miguel A. Ruz <miguelruz@yahoo.com>
2018-12-09 23:10:36 +0100

    Create mapper.py

---
+++ MapReduce/mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""POrograma mapper.py"""
+
+import sys
+
+# la entrada de datos llega desde STDIN (standard input)
+for line in sys.stdin:
+    #eliminamos los espacios en blanco iniciales y finales
+    line = line.strip()
+    # dividimos la línea en palabras
+    words = line.split()
+    # Para cada palabra encontrada
+    for word in words:
+        # escribir los resultados en STDOUT (salida estándar);
+        print '%s\t%s' % (word, 1)
