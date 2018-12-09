41b92047d38a266ebd54b6e74889a3f9b0fd04aa
Miguel A. Ruz <miguelruz@yahoo.com>
2018-12-09 23:48:05 +0100

    Daily exchange Rate
    Daily exchange Rate

---
+++ MapReduce/daily-exchange-rate/mapper.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+# The Mapper
+import sys
+import csv
+# Set local variables
+iteration = 0
+currentCountry = None
+previousCountry = None
+currentFx = None
+previousFx = None
+percentChange = None
+currentKey = None
+fxMap = []
+
+infile = sys.stdin
+next(infile) 
+for line in infile:
+line = line.strip()
+    line = line.split(',', 2)
+try:
+
+        currentCountry = line[1].rstrip()
+        if len(line[2]) == 0:
+            continue
+        currentFx = float(line[2])
+if currentCountry != previousCountry:
+            previousCountry = currentCountry
+            previousFx = currentFx
+            previousLine = line
+            continue
+# Si el país es el mismo que el anterior, lo añadimos
+        elif currentCountry == previousCountry:
+            percentChange = ((currentFx - previousFx) / previousFx) * 100.00
+            percentChange = round(percentChange, 2)
+            percentChange = percentChange
+currentKey = "%s: %6.2f%%" % (currentCountry, percentChange)
+
+            fxMap.append(tuple([currentKey, 1]))
+
+        previousCountry = currentCountry
+        previousFx = currentFx
+        previousLine = line
+
+    except Exception as e:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(e).__name__, e.args)
+        print "currentFx: %.2f previousFx: %.2f" % (currentFx, previousFx)
+        print message
+        sys.exit(0)
+
+for i in sorted(fxMap):
+    print "%-20s - %d" % (i[0], i[1])
