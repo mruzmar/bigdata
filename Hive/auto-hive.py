c7d53364cdd082db93fbc16577214b2009466449
Miguel A. Ruz <miguel.ruz.mar@gmail.com>
2021-03-05 10:24:10 +0100

    Update auto-hive.py

--- Hive/auto-hive.py
+++ Hive/auto-hive.py
@@ -17,39 +17,34 @@ def run_hive_query(query):
 # pasamos fichero a stating
 cmd="wget https://download.microsoft.com/download/7/F/3/7F35E2C0-B84F-4B45-A9F2-2A888D51C1D9/sample.log"
 print ("Ejecutando "+cmd)
-execute = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = execute.run(cmd)
+output = subprocess.check_output(cmd)
 
 cmd="hdfs dfs -mkdir -p /datalake/logs/mobile/application1"
 print ("Ejecutando "+cmd)
-execute = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = execute.run(cmd)
+output = subprocess.check_output(cmd)
 
 
 cmd="hdfs dfs -put sample.log /datalake/logs/mobile/application1/"
 print ("Ejecutando "+cmd)
-execute = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = execute.run(cmd)
+output = subprocess.check_output(cmd)
 
 
 
 cmd="""hive -S -e "CREATE EXTERNAL TABLE log4jLogs (t1 string, t2 string, t3 string, t4 string, t5 string, t6 string, t7 string) ROW FORMAT DELIMITED FIELDS TERMINATED BY \" \" STORED AS TEXTFILE LOCATION \"/home/info/\";" """
-execute = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = execute.run(cmd)
+print ("Ejecutando "+cmd)
+output = subprocess.check_output(cmd)
 
 
 
 
 cmd="""hive -S -e "LOAD DATA INPATH '/datalake/logs/mobile/application1/sample.log' OVERWRITE INTO TABLE log4jLogs;" """
 print ("Ejecutando "+cmd)
-execute = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = execute.run(cmd)
+output = subprocess.check_output(cmd)
 
 
 cmd = """ hive -S -e "SELECT * FROM log4jLogs WHERE t4 = '[ERROR]';" """
 print ("Ejecutando "+cmd)
-execute = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, err = execute.run(cmd)
+output = subprocess.check_output(cmd)
 
 print (output)
 
